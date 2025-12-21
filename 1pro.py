2a4f2724ae2e339e6bc54f6f4c872202ae903cbc
ymanish1475-del <ymanish1475@gmail.com>
2025-12-21 16:30:52 +0530

    first commit

---
+++ 1pro.py
@@ -0,0 +1,23 @@
+# manish="kay re"
+# name="manish"
+# age =19
+# point =  1
+# print(manish)
+# print(name)
+# print(age)
+# print (point)
+manish="kay re"
+name="manish"
+age =19
+point =  1
+print(manish)
+print(age)
+print (point)
+print(name)
+print("manish is my name "" my age is 19")
+print(78+7+388+9033)
+print(10/9)
+print (69/3)
+print (9*738)
+print (37-87-98)
+print(678-78)
---
+++ 2pro.py
@@ -0,0 +1,35 @@
+name="manish"
+age =19
+point =1
+print ("my name is :", name)
+print ("my age is :",age)
+print ("my point is :",point)
+# name = "manish "
+# press =name
+# book= press
+# print(book)
+# name = "manish "
+# age =19
+# point=1.000
+# press =name
+# book= press
+# print(book)
+# print(type(name))
+# print(type(point))
+# print(type(age))
+# name1='man'
+# name2="man"
+# name3='''man'''
+# print(name3)
+# print(name2)
+# print(name1
+# b=78
+# d=98
+# c=45
+# Sum=b+c+d
+# print(Sum)
+# b=780
+# d=98
+# c=45
+# diff=b-c-d
+# print(diff)
