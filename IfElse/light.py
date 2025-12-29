6eaadeb50871cf5f7b115911a4d4caa754597ca7
ymanish1475-del <ymanish1475@gmail.com>
2025-12-29 21:32:50 +0530

    NEW LEARNINGS

---
+++ IfElse/light.py
@@ -0,0 +1,21 @@
+# light = input("light color : ")
+
+# if(light ==  "red "):
+#         print("stop")
+# elif(light == "yellow"):
+#         print("look")
+# elif(light == "green"):
+#         print("go")
+# else:
+#         print("light is broken")
+
+light=input("light color:")
+
+if(light=="red"):
+    print("aap nahi ja sakte hea")
+elif(light=="yellow"):
+    print("ruko jara sabar karo")
+elif(light=="green"):
+    print("aap ja sakte hea")
+else:
+    print("signal kharab hea")
---
+++ IfElse/marks.py
@@ -0,0 +1,19 @@
+marks=int(input("kitte marks hea tere"))
+
+if(marks>=90):
+    print(" sir ye jo aap kar rahe hea na theek nahi kar rahe hea")
+
+elif(marks>=80):
+    print(" dekh raha hea vinod.... ")
+
+elif(marks>=70):
+    print("ek kaam kro chhod do IAS ")
+
+elif(marks>=60):
+    print("re chhokra ganwar nikal gya")
+
+elif(marks>=50):
+    print("kyon nahi ho rahi padayi")
+
+else:
+    print("kaun gooola se hoooo....")
