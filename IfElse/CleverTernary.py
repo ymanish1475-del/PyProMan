b17f5f0222d28eaf3e176757dad6920e02ba48a0
ymanish1475-del <ymanish1475@gmail.com>
2026-01-03 09:24:30 +0530

    LEARN CLEVER TERNARY STATEMENT

---
+++ IfElse/CleverTernary.py
@@ -0,0 +1,21 @@
+# cleaver ternary
+
+# age= int(input("age:"))
+# # (false val, true value)
+# vote=("no","yes"  )[age>=18]
+# print(vote)
+
+
+# sal=int(input("sal:"))
+
+# tax=sal*(0.1,0.2)[sal<=500]
+
+# print(tax)
+
+# simle interest
+
+p =int (input("a:"))
+t=int (input("a:"))
+r =int (input("a:"))
+si=(p*t*r/100)
+print(si)
